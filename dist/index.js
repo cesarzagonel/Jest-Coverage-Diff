b84e7d39cc549941223d6f2b49be15cb2ab99311
Cesar Zagonel <zagonelcesar@gmail.com>
2021-12-06 21:05:54 -0400

    revert back

--- dist/index.js
+++ dist/index.js
@@ -2043,9 +2043,9 @@ function run() {
             let commentId = null;
             child_process_1.execSync(commandToRun);
             const codeCoverageNew = (JSON.parse(fs_1.default.readFileSync('coverage-summary.json').toString()));
-            child_process_1.execSync(`/usr/bin/git switch ${branchNameBase}`);
-            // execSync('/usr/bin/git stash')
-            // execSync(`/usr/bin/git checkout --force ${branchNameBase}`)
+            child_process_1.execSync('/usr/bin/git fetch');
+            child_process_1.execSync('/usr/bin/git stash');
+            child_process_1.execSync(`/usr/bin/git checkout --progress --force ${branchNameBase}`);
             if (commandAfterSwitch) {
                 child_process_1.execSync(commandAfterSwitch);
             }
--- src/main.ts
+++ src/main.ts
@@ -30,9 +30,9 @@ async function run(): Promise<void> {
     const codeCoverageNew = <CoverageReport>(
       JSON.parse(fs.readFileSync('coverage-summary.json').toString())
     )
-    execSync(`/usr/bin/git switch ${branchNameBase}`)
-    // execSync('/usr/bin/git stash')
-    // execSync(`/usr/bin/git checkout --force ${branchNameBase}`)
+    execSync('/usr/bin/git fetch')
+    execSync('/usr/bin/git stash')
+    execSync(`/usr/bin/git checkout --progress --force ${branchNameBase}`)
     if (commandAfterSwitch) {
       execSync(commandAfterSwitch)
     }
